8d33703bfc8d3507cdbb161f503d96daebee1222
Aniket <aniketbhuse21@gmail.com>
2026-05-01 23:08:38 +0530

    Controller chnages

--- HealthcareApp/obj/Debug/net8.0/HealthcareApp.AssemblyInfo.cs
+++ HealthcareApp/obj/Debug/net8.0/HealthcareApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882112d592a2b936573b629d16e98fdda5762667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad7f0bbfa66bb237eecac76bc98a2345f09f751")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
